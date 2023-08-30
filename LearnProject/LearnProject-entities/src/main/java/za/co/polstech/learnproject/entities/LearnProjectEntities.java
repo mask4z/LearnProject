76a24da434ad704283da0114f633f15a52fafe50
Renaldo Swanepoel <renaldo@ibureau.services>
2023-08-30 07:15:23 +0200

    Start of Jakarta Tutorial project

---
+++ LearnProject/LearnProject-entities/src/main/java/za/co/polstech/learnproject/entities/LearnProjectEntities.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package za.co.polstech.learnproject.entities;
+
+/**
+ *
+ * @author R3n
+ */
+public class LearnProjectEntities {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
